0325e29ffb98e015e992e1c834abd5d7ffd82667
Otaconned <144296987+Otaconned@users.noreply.github.com>
2024-10-11 14:49:52 -0400

    Create Midterm.js

---
+++ Midterm.js
@@ -0,0 +1,11 @@
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let duration = [6,7,8,9,5,6,4];
+
+let $ = function(id) { return document.querySelector("#"+id); };
+
+document.addEventListener("DOMContentLoaded", function() {
+    //event handlers here
+
+});
+
+// define functions here
