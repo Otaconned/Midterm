b7210bdba806a02eda373599040d71bbe60ca64b
Otaconned <144296987+Otaconned@users.noreply.github.com>
2024-10-11 15:37:26 -0400

    Update Midterm.js

--- Midterm.js
+++ Midterm.js
@@ -20,25 +20,67 @@ function updateSleep(){
 
     if(isNaN(sleepDuration) || !dayOfWk) {
         alert("Enter a valid number for sleep duration");
-        $('#sleepDuration').value = "";
+        $("#sleepDuration").value = "";
     } else {
-        alert("Your updated sleep duration is " + hours + " hrs on " + day);
-        $('#sleepDuration').value = "";
+        let dayIndex = days.indexOf(dayOfWk.value);
+        duration[dayIndex] = hours;
+
+        alert("Your updated sleep duration is " + hours + " hrs on " + days[i]);
+        $("#sleepDuration").value = "";
     }
 }
 
 function ShowAverageMinMaxSleep() {
     let totalSleep = 0;
-    const average = (totalSleep/duration.length).toFixed(2);
-    const min = Math.min(duration[i]);
-    const max = Math.max(duration[i]);
-    const sleepLabel = document.createElement('label');
+    const minSleep = Math.min(...duration);
+    const maxSleep = Math.max(...duration);
 
     for (let i = 0; i < duration.length; i ++) {
         totalSleep += duration[i];
     }
 
-    $("averageSleep").textContent = "Average sleep duration for this week";
+    const averageSleep = (totalSleep/duration.length).toFixed(2);
+
+    $("averageSleep").value = "Average sleep duration for this week";
     $("averageSleep").style.color = "green";
     $("averageSleep").style.borderColor = "red";
+}
+
+function displaySleepDuration() {
+    let table = $("sleep_table");
+    table.innerHTML = "";
+
+    let username = $("#username").value;
+
+    let tableParagraph = document.createElement("p");
+    tableParagraph.textContent = "Hey " + username + "! You slept less than five hours on the following days";
+    table.appendChild(tableParagraph);
+
+    let headerRow = document.createElement("tr");
+
+    let nameHeader = document.createElement("th");
+    nameHeader.textContent="Day";
+    headerRow.appendChild(nameHeader);
+
+    let hourHeader = document.createElement("th");
+    hourHeader.textContent="Hour";
+    headerRow.appendChild(hourHeader);
+
+    table.appendChild(headerRow);
+
+    for (let i = 0; i < days.length; i++){
+        if(duration[i] < 5) {
+            let row = document.createElement("tr");
+
+            let daysColumn = document.createElement("td");
+            daysColumn.textContent = days[i];
+            row.appendChild(daysColumn);
+    
+            let hoursColumn = document.createElement("td");
+            hoursColumn.textContent = duration[i];
+            row.appendChild(hoursColumn);
+    
+            table.appendChild(row);  
+        }
+    }
 }
